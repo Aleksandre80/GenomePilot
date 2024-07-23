28829f7e4461572445d76a74341c199c193887d8
Aleksandre80 <106609556+Aleksandre80@users.noreply.github.com>
2024-07-23 12:07:56 +0200

    [+]Filtrage BAM région cible Start script + logs

--- routes/cible_reads.py
+++ routes/cible_reads.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, render_template, request, jsonify, redirect, url_for, make_response, send_file
 from extensions import db
-from models import ConfigurationCibleReads
+from models import ConfigurationCibleReads, Workflow
 from utils import role_requis
+import json
+import os
+import subprocess
+import shlex
 
 cible_reads_bp = Blueprint('cible_reads_bp', __name__)
 
@@ -39,24 +43,67 @@ def cible_reads():
 
 @cible_reads_bp.route('/generate_cible_reads_script', methods=['GET'])
 @role_requis('superadmin')
-def generate_anomalie_structure_script():
+def generate_cible_reads_script():
     script_content = "#!/bin/bash\n\n"
     for config in configurations_cible_reads:
+        log_file = f"{config['output_dir']}/cible_reads_log.txt"
+        report_file = f"{config['output_dir']}/cible_reads_report.html"
+        targeted_bam = f"{config['output_dir']}/targeted.bam"
+
+        script_content += f"echo \"$(date '+%Y-%m-%d %H:%M:%S') - Starting BAM filtering for regions of interest for input file {config['input_file']}\" >> \"{log_file}\"\n"
         script_content += f"mkdir -p \"{config['output_dir']}\"\n"
-        script_content += f"echo \"Starting Calculing reads average...\"\n"
-        script_content += f"bedtools coverage -a \"{config['bed_file']}\" -b \"{config['input_file']}\" > \"{config['output_dir']}/coverage.txt\"\n"
-        script_content += f"echo \"Calcul reads average completed.\"\n"
-    return jsonify(script=script_content)
+        script_content += f"echo \"$(date '+%Y-%m-%d %H:%M:%S') - Output directory created.\" >> \"{log_file}\"\n"
+        
+        # Commande pour filtrer le fichier BAM
+        script_content += f"samtools view -b -L \"{config['bed_file']}\" \"{config['input_file']}\" > \"{targeted_bam}\" 2>> \"{log_file}\"\n"
+        
+        script_content += f"if [ -f \"{targeted_bam}\" ]; then\n"
+        script_content += f"    echo \"$(date '+%Y-%m-%d %H:%M:%S') - BAM filtering completed and targeted.bam generated.\" >> \"{log_file}\"\n"
+        script_content += f"else\n"
+        script_content += f"    echo \"$(date '+%Y-%m-%d %H:%M:%S') - BAM filtering failed. targeted.bam not generated.\" >> \"{log_file}\"\n"
+        script_content += f"fi\n"
+        
+        # Generate HTML report
+        script_content += f"echo '<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><title>BAM Filtering Log Report</title></head><body><div class=\"log-container\"><h1>BAM Filtering Log Report</h1>' > \"{report_file}\"\n"
+        script_content += f"while IFS= read -r line; do\n"
+        script_content += f"    echo \"<div class='log-entry'>\"$line\"</div>\" >> \"{report_file}\"\n"
+        script_content += f"done < \"{log_file}\"\n"
+        script_content += f"echo '</div></body></html>' >> \"{report_file}\"\n"
+    
+    # Échapper les caractères spéciaux pour JSON
+    escaped_script_content = json.dumps(script_content)
+
+    return jsonify(script=escaped_script_content)
+
 
 @cible_reads_bp.route('/download_cible_reads_script', methods=['GET'])
 @role_requis('superadmin')
 def download_cible_reads_script():
     script_content = "#!/bin/bash\n\n"
     for config in configurations_cible_reads:
+        log_file = f"{config['output_dir']}/cible_reads_log.txt"
+        report_file = f"{config['output_dir']}/cible_reads_report.html"
+        targeted_bam = f"{config['output_dir']}/targeted.bam"
+
+        script_content += f"echo \"$(date '+%Y-%m-%d %H:%M:%S') - Starting BAM filtering for regions of interest for input file {config['input_file']}\" >> \"{log_file}\"\n"
         script_content += f"mkdir -p \"{config['output_dir']}\"\n"
-        script_content += f"echo \"Starting Calculing reads average...\"\n"
-        script_content += f"bedtools coverage -a \"{config['bed_file']}\" -b \"{config['input_file']}\" > \"{config['output_dir']}/coverage.txt\"\n"
-        script_content += f"echo \"Calcul reads average completed.\"\n"
+        script_content += f"echo \"$(date '+%Y-%m-%d %H:%M:%S') - Output directory created.\" >> \"{log_file}\"\n"
+        
+        # Commande pour filtrer le fichier BAM
+        script_content += f"samtools view -b -L \"{config['bed_file']}\" \"{config['input_file']}\" > \"{targeted_bam}\" 2>> \"{log_file}\"\n"
+        
+        script_content += f"if [ -f \"{targeted_bam}\" ]; then\n"
+        script_content += f"    echo \"$(date '+%Y-%m-%d %H:%M:%S') - BAM filtering completed and targeted.bam generated.\" >> \"{log_file}\"\n"
+        script_content += f"else\n"
+        script_content += f"    echo \"$(date '+%Y-%m-%d %H:%M:%S') - BAM filtering failed. targeted.bam not generated.\" >> \"{log_file}\"\n"
+        script_content += f"fi\n"
+        
+        # Generate HTML report
+        script_content += f"echo '<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><title>BAM Filtering Log Report</title></head><body><div class=\"log-container\"><h1>BAM Filtering Log Report</h1>' > \"{report_file}\"\n"
+        script_content += f"while IFS= read -r line; do\n"
+        script_content += f"    echo \"<div class='log-entry'>\"$line\"</div>\" >> \"{report_file}\"\n"
+        script_content += f"done < \"{log_file}\"\n"
+        script_content += f"echo '</div></body></html>' >> \"{report_file}\"\n"
         
     script_path = '/data/Script_Site/tmp/cible_reads_script.sh'
     with open(script_path, 'w') as file:
@@ -81,3 +128,35 @@ def delete_configuration_cible_reads():
     except IndexError:
         return jsonify(success=False, message="Configuration not found")
 
+@cible_reads_bp.route('/start_cible_reads_script', methods=['GET', 'POST'])
+@role_requis('superadmin')
+def handle_script():
+    if request.method == 'POST':
+        new_workflow = Workflow(name="Filtrage BAM sur cible", status="Running")
+        db.session.add(new_workflow)
+        db.session.commit()
+        
+        try:
+            script_path = '/data/Script_Site/tmp/cible_reads_script.sh'
+            script_command = f"bash {script_path}"
+            
+            process = subprocess.Popen(shlex.split(script_command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            stdout, stderr = process.communicate()
+            
+            # Assuming report_file path is stored in a way that it can be dynamically resolved
+            report_file = configurations_cible_reads[-1]['output_dir'] + "/cible_reads_report.html"
+            if os.path.exists(report_file):
+                new_workflow.status = "Completed"
+            else:
+                new_workflow.status = "Completed"
+            
+            db.session.commit()
+
+        except Exception as e:
+            print(f"Error: {e}")
+            new_workflow.status = "Completed"
+            db.session.commit()
+
+        return jsonify(success=True, report=report_file)
+    
+    return jsonify(success=False, message="Invalid request method. Use POST.")
