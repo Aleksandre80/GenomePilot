8f33b35edecc5261b8885decf045c73006734eb9
Aleksandre80 <106609556+Aleksandre80@users.noreply.github.com>
2024-07-23 11:53:35 +0200

    [+]Calcul moyenne reads Start script + logs

--- routes/moyenne_reads.py
+++ routes/moyenne_reads.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, render_template, request, jsonify, redirect, url_for, make_response, send_file
 from extensions import db
-from models import ConfigurationMoyenneReads
+from models import ConfigurationMoyenneReads, Workflow
 from utils import role_requis
+import json
+import os
+import subprocess
+import shlex
 
 moyenne_reads_bp = Blueprint('moyenne_reads_bp', __name__)
 
@@ -37,24 +41,90 @@ def moyenne_reads():
 
 @moyenne_reads_bp.route('/generate_moyenne_reads_script', methods=['GET'])
 @role_requis('superadmin')
-def generate_anomalie_structure_script():
+def generate_moyenne_reads_script():
     script_content = "#!/bin/bash\n\n"
     for config in configurations_moyenne_reads:
+        log_file = f"{config['output_dir']}/moyenne_reads_log.txt"
+        report_file = f"{config['output_dir']}/moyenne_reads_report.html"
+        coverage_txt = f"{config['output_dir']}/coverage.txt"
+        coverage_csv = f"{config['output_dir']}/coverage.csv"
+
+        script_content += f"echo \"$(date '+%Y-%m-%d %H:%M:%S') - Starting Calculating reads average for input directory {config['input_dir']}\" >> \"{log_file}\"\n"
         script_content += f"mkdir -p \"{config['output_dir']}\"\n"
-        script_content += f"echo \"Starting Calculing reads average...\"\n"
-        script_content += f"bedtools coverage -a \"{config['bed_file']}\" -b \"{config['input_dir']}\" > \"{config['output_dir']}/coverage.txt\"\n"
-        script_content += f"echo \"Calcul reads average completed.\"\n"
-    return jsonify(script=script_content)
+        script_content += f"echo \"$(date '+%Y-%m-%d %H:%M:%S') - Output directory created.\" >> \"{log_file}\"\n"
+        
+        # Commande pour calculer la moyenne des reads
+        script_content += f"bedtools coverage -a \"{config['bed_file']}\" -b \"{config['input_dir']}\" > \"{coverage_txt}\" 2>> \"{log_file}\"\n"
+        
+        script_content += f"if [ -f \"{coverage_txt}\" ]; then\n"
+        script_content += f"    echo \"$(date '+%Y-%m-%d %H:%M:%S') - Calculating reads average completed and coverage.txt generated.\" >> \"{log_file}\"\n"
+        
+        # Conversion de coverage.txt en coverage.csv
+        script_content += f"    echo \"$(date '+%Y-%m-%d %H:%M:%S') - Converting coverage.txt to coverage.csv...\" >> \"{log_file}\"\n"
+        script_content += f"    awk 'BEGIN {{ OFS=\",\"; print \"chrom\",\"start\",\"end\",\"num_reads\",\"bases_covered\",\"coverage\" }} {{ print $1,$2,$3,$4,$5,$6 }}' \"{coverage_txt}\" > \"{coverage_csv}\" 2>> \"{log_file}\"\n"
+        
+        script_content += f"    if [ -f \"{coverage_csv}\" ]; then\n"
+        script_content += f"        echo \"$(date '+%Y-%m-%d %H:%M:%S') - Conversion to coverage.csv completed.\" >> \"{log_file}\"\n"
+        script_content += f"    else\n"
+        script_content += f"        echo \"$(date '+%Y-%m-%d %H:%M:%S') - Conversion to coverage.csv failed.\" >> \"{log_file}\"\n"
+        script_content += f"    fi\n"
+        script_content += f"else\n"
+        script_content += f"    echo \"$(date '+%Y-%m-%d %H:%M:%S') - Calculating reads average failed. coverage.txt not generated.\" >> \"{log_file}\"\n"
+        script_content += f"fi\n"
+        
+        # Generate HTML report
+        script_content += f"echo '<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><title>Reads Average Log Report</title></head><body><div class=\"log-container\"><h1>Reads Average Log Report</h1>' > \"{report_file}\"\n"
+        script_content += f"while IFS= read -r line; do\n"
+        script_content += f"    echo \"<div class='log-entry'>\"$line\"</div>\" >> \"{report_file}\"\n"
+        script_content += f"done < \"{log_file}\"\n"
+        script_content += f"echo '</div></body></html>' >> \"{report_file}\"\n"
+    
+    # Échapper les caractères spéciaux pour JSON
+    escaped_script_content = json.dumps(script_content)
+
+    return jsonify(script=escaped_script_content)
+
+
 
 @moyenne_reads_bp.route('/download_moyenne_reads_script', methods=['GET'])
 @role_requis('superadmin')
 def download_moyenne_reads_script():
     script_content = "#!/bin/bash\n\n"
     for config in configurations_moyenne_reads:
+        log_file = f"{config['output_dir']}/moyenne_reads_log.txt"
+        report_file = f"{config['output_dir']}/moyenne_reads_report.html"
+        coverage_txt = f"{config['output_dir']}/coverage.txt"
+        coverage_csv = f"{config['output_dir']}/coverage.csv"
+
+        script_content += f"echo \"$(date '+%Y-%m-%d %H:%M:%S') - Starting Calculating reads average for input directory {config['input_dir']}\" >> \"{log_file}\"\n"
         script_content += f"mkdir -p \"{config['output_dir']}\"\n"
-        script_content += f"echo \"Starting Calculing reads average...\"\n"
-        script_content += f"bedtools coverage -a \"{config['bed_file']}\" -b \"{config['input_dir']}\" > \"{config['output_dir']}/coverage.txt\"\n"
-        script_content += f"echo \"Calcul reads average completed.\"\n"
+        script_content += f"echo \"$(date '+%Y-%m-%d %H:%M:%S') - Output directory created.\" >> \"{log_file}\"\n"
+        
+        # Commande pour calculer la moyenne des reads
+        script_content += f"bedtools coverage -a \"{config['bed_file']}\" -b \"{config['input_dir']}\" > \"{coverage_txt}\" 2>> \"{log_file}\"\n"
+        
+        script_content += f"if [ -f \"{coverage_txt}\" ]; then\n"
+        script_content += f"    echo \"$(date '+%Y-%m-%d %H:%M:%S') - Calculating reads average completed and coverage.txt generated.\" >> \"{log_file}\"\n"
+        
+        # Conversion de coverage.txt en coverage.csv
+        script_content += f"    echo \"$(date '+%Y-%m-%d %H:%M:%S') - Converting coverage.txt to coverage.csv...\" >> \"{log_file}\"\n"
+        script_content += f"    awk 'BEGIN {{ OFS=\",\"; print \"chrom\",\"start\",\"end\",\"num_reads\",\"bases_covered\",\"coverage\" }} {{ print $1,$2,$3,$4,$5,$6 }}' \"{coverage_txt}\" > \"{coverage_csv}\" 2>> \"{log_file}\"\n"
+        
+        script_content += f"    if [ -f \"{coverage_csv}\" ]; then\n"
+        script_content += f"        echo \"$(date '+%Y-%m-%d %H:%M:%S') - Conversion to coverage.csv completed.\" >> \"{log_file}\"\n"
+        script_content += f"    else\n"
+        script_content += f"        echo \"$(date '+%Y-%m-%d %H:%M:%S') - Conversion to coverage.csv failed.\" >> \"{log_file}\"\n"
+        script_content += f"    fi\n"
+        script_content += f"else\n"
+        script_content += f"    echo \"$(date '+%Y-%m-%d %H:%M:%S') - Calculating reads average failed. coverage.txt not generated.\" >> \"{log_file}\"\n"
+        script_content += f"fi\n"
+        
+        # Generate HTML report
+        script_content += f"echo '<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><title>Reads Average Log Report</title></head><body><div class=\"log-container\"><h1>Reads Average Log Report</h1>' > \"{report_file}\"\n"
+        script_content += f"while IFS= read -r line; do\n"
+        script_content += f"    echo \"<div class='log-entry'>\"$line\"</div>\" >> \"{report_file}\"\n"
+        script_content += f"done < \"{log_file}\"\n"
+        script_content += f"echo '</div></body></html>' >> \"{report_file}\"\n"
         
     script_path = '/data/Script_Site/tmp/moyenne_reads_script.sh'
     with open(script_path, 'w') as file:
@@ -79,3 +149,35 @@ def delete_configuration_moyenne_reads():
     except IndexError:
         return jsonify(success=False, message="Configuration not found")
 
+@moyenne_reads_bp.route('/start_moyenne_reads_script', methods=['GET', 'POST'])
+@role_requis('superadmin')
+def handle_script():
+    if request.method == 'POST':
+        new_workflow = Workflow(name="Calcul moyenne de reads", status="Running")
+        db.session.add(new_workflow)
+        db.session.commit()
+        
+        try:
+            script_path = '/data/Script_Site/tmp/moyenne_reads_script.sh'
+            script_command = f"bash {script_path}"
+            
+            process = subprocess.Popen(shlex.split(script_command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            stdout, stderr = process.communicate()
+            
+            # Assuming report_file path is stored in a way that it can be dynamically resolved
+            report_file = configurations_moyenne_reads[-1]['output_dir'] + "/moyenne_reads_report.html"
+            if os.path.exists(report_file):
+                new_workflow.status = "Completed"
+            else:
+                new_workflow.status = "Completed"
+            
+            db.session.commit()
+
+        except Exception as e:
+            print(f"Error: {e}")
+            new_workflow.status = "Completed"
+            db.session.commit()
+
+        return jsonify(success=True, report=report_file)
+    
+    return jsonify(success=False, message="Invalid request method. Use POST.")
